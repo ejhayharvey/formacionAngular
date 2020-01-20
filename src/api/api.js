6d28840cead3681bcba5dfe91f72d3b20afc9454
Emil Jay Averion Barradas <eaverion@everis.com>
2020-01-20 12:41:40 +0100

    server updated

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express();
 
 app.use(cors())
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
