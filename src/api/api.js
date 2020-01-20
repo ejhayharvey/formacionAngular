b04a676faaec316d515a088b8df2e7698f5a02fa
Emil Jay Averion Barradas <eaverion@everis.com>
2020-01-20 13:44:21 +0100

    dir updated

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist/ProjectAngular'));
 
 app.get('/*', function(req,res) {ProjectAngular
 
-res.sendFile(path.join(__dirname+'/dist/ProjectAngular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
